e212db9b3b4a19e935ed1df0b9e1de9f2a61699c
Yavuz OKTAY <yavuzoktay@MacBook-Pro-8.local>
2025-07-25 23:08:12 +0300

    Update all code, exclude .env files

--- server/index.js
+++ server/index.js
@@ -1825,7 +1825,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
