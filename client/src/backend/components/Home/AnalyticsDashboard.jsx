a656eeb2793e156094a1225b179c0f8be398c67a
Yavuz OKTAY <yavuzoktay@MacBook-Pro-8.local>
2025-07-25 23:24:20 +0300

    Update all code, exclude .env files

--- client/src/backend/components/Home/AnalyticsDashboard.jsx
+++ client/src/backend/components/Home/AnalyticsDashboard.jsx
@@ -11,7 +11,7 @@ const AnalyticsDashboard = ({ dateRange }) => {
 
     useEffect(() => {
         setLoading(true);
-        axios.get(`http://3.95.28.43:3002/api/analytics`, {
+        axios.get(`http://3.95.28.43:3000/api/analytics`, {
             params: {
                 start_date: dateRange.start,
                 end_date: dateRange.end
